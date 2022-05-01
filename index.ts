bf89b45fde642a6aa662b8c0a029b800bf12e280
Saif Ali <Saifincreation@gmail.com>
2022-05-01 14:22:47 +0200

    refactor: add mongodb error handling, remove body-parser

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser"
 import mongoose from "mongoose";
 
 import { AdminRoute, VendorRoute } from "./routes";
@@ -7,18 +6,51 @@ import { MONGO_URI } from "./config";
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/admin", AdminRoute);
 app.use("/vendor", VendorRoute);
 
-mongoose.connect(MONGO_URI).then(result => {
-	console.log("DB Connected.");
-}).catch(err => {
-	console.log(`DB Error: ${err}`);
+const dbReconnectTimeout = 5000; // ms.
+
+function MongoDBConnect() {
+  mongoose.connect(MONGO_URI)
+    .catch(() => {}); // Catch the warning, no further treatment is required
+                      // because the Connection events are already doing this
+                      // for us.
+}
+
+const db = mongoose.connection;
+
+db.on('connecting', () => {
+  console.info('Connecting to MongoDB...');
 });
+
+db.on('error', (error) => {
+  console.error(`MongoDB connection error: ${error}`);
+  mongoose.disconnect();
+});
+
+db.on('connected', () => {
+  console.info('Connected to MongoDB!');
+});
+
+db.once('open', () => {
+  console.info('MongoDB connection opened!');
+});
+
+db.on('reconnected', () => {
+  console.info('MongoDB reconnected!');
+});
+
+db.on('disconnected', () => {
+  console.error(`MongoDB disconnected! Reconnecting in ${dbReconnectTimeout / 1000}s...`);
+  setTimeout(() => MongoDBConnect(), dbReconnectTimeout);
+});
+
+MongoDBConnect();
 
 app.listen(8000, () => {
 	
