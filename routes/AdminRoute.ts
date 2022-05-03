c8a7b0f6d0acb2c3d12fb0e6a828f027669cc383
Saif Ali <Saifincreation@gmail.com>
2022-05-04 01:42:50 +0200

    refactor: cleanup

--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -3,13 +3,13 @@ import { CreateVendor, GetVendorByID, GetVendors } from "../controllers";
 
 const router = express.Router();
 
-router.post("/vendor", CreateVendor);
-router.get("/vendors", GetVendors);
-router.get("/vendor/:id", GetVendorByID);
-
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 	return res.json({ message: "Hello from Admin."});
 });
+
+router.post("/vendor", CreateVendor);
+router.get("/vendors", GetVendors);
+router.get("/vendor/:id", GetVendorByID);
 
 export { router as AdminRoute };
