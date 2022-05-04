59e00c3e0903355283ec6d521476a79c11c3f50a
Saif Ali <Saifincreation@gmail.com>
2022-05-04 02:00:04 +0200

    refactor: extract out db and express instances

--- index.ts
+++ index.ts
@@ -1,61 +1,19 @@
 import express from "express";
-import mongoose from "mongoose";
+import App from "./services/ExpressApp"
+import Database from "./services/Database";
 
-import { AdminRoute, VendorRoute } from "./routes";
-import { MONGO_URI } from "./config";
-import path from "path";
+const startServer = async () => {
+	
+	const app = express();
 
-const app = express();
+	await Database();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/images", express.static(path.join(__dirname, "images")));
+	await App(app);
 
+	app.listen(8000, () => {
 
-app.use("/admin", AdminRoute);
-app.use("/vendor", VendorRoute);
-
-const dbReconnectTimeout = 5000; // ms.
-
-function MongoDBConnect() {
-  mongoose.connect(MONGO_URI)
-    .catch(() => {}); // Catch the warning, no further treatment is required
-                      // because the Connection events are already doing this
-                      // for us.
+		console.log("Listening on port 8000.");
+	});
 }
 
-const db = mongoose.connection;
-
-db.on('connecting', () => {
-  console.info('Connecting to MongoDB...');
-});
-
-db.on('error', (error) => {
-  console.error(`MongoDB connection error: ${error}`);
-  mongoose.disconnect();
-});
-
-db.on('connected', () => {
-  console.info('Connected to MongoDB!');
-});
-
-db.once('open', () => {
-  console.info('MongoDB connection opened!');
-});
-
-db.on('reconnected', () => {
-  console.info('MongoDB reconnected!');
-});
-
-db.on('disconnected', () => {
-  console.error(`MongoDB disconnected! Reconnecting in ${dbReconnectTimeout / 1000}s...`);
-  setTimeout(() => MongoDBConnect(), dbReconnectTimeout);
-});
-
-MongoDBConnect();
-
-app.listen(8000, () => {
-	
-	console.clear();
-	console.log("Listening at 8000");
-});
+startServer();
---
+++ services/Database.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "../config";
+
+const dbReconnectTimeout = 5000; // ms.
+
+export default async () => {
+
+	function MongoDBConnect() {
+		mongoose.connect(MONGO_URI)
+		  .catch(() => {}); // Catch the warning, no further treatment is required
+							// because the Connection events are already doing this
+							// for us.
+	  }
+	  
+	  const db = mongoose.connection;
+	  
+	  db.on('connecting', () => {
+		console.info('Connecting to MongoDB...');
+	  });
+	  
+	  db.on('error', (error) => {
+		console.error(`MongoDB connection error: ${error}`);
+		mongoose.disconnect();
+	  });
+	  
+	  db.on('connected', () => {
+		console.info('Connected to MongoDB!');
+	  });
+	  
+	  db.once('open', () => {
+		console.info('MongoDB connection opened!');
+	  });
+	  
+	  db.on('reconnected', () => {
+		console.info('MongoDB reconnected!');
+	  });
+	  
+	  db.on('disconnected', () => {
+		console.error(`MongoDB disconnected! Reconnecting in ${dbReconnectTimeout / 1000}s...`);
+		setTimeout(() => MongoDBConnect(), dbReconnectTimeout);
+	  });
+	  
+	  MongoDBConnect();
+}
