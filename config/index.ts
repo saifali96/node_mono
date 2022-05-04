5994a375201799e9593a677b15765d55bd125929
Saif Ali <Saifincreation@gmail.com>
2022-05-04 13:15:51 +0200

    Move to mongodb 5.0.8

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,2 @@
-export const MONGO_URI = "mongodb://localhost:27017/node_mono";
+export const MONGO_URI = "mongodb://localhost:27017/node_mono_latest";
 export const JWT_SECRET = "Sup3rs4f3_JWT_S33cr3t!";
