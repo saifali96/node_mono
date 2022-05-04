4f908a2ecd80604db5397dc9cf0c7759022fadf3
Saif Ali <Saifincreation@gmail.com>
2022-05-04 15:26:25 +0200

    refactor: rename pincode to zipcode

---
+++ routes/ShoppingRoute.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response, NextFunction } from "express";
+
+const router = express.Router();
+
+// Food Availability
+// Top Restaurants
+// Available in 30 mins
+// Search Food
+// Find Restaurant by ID
+
+export { router as ShoppingRoute };
