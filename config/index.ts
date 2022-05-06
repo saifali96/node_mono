ceb24ad6e2302b803761628bb88a8deb1917864e
Saif Ali <Saifincreation@gmail.com>
2022-05-06 13:44:23 +0200

    refactor env vars import

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
-export const MONGO_URI = `${process.env.MONGO_URI}`;
-export const JWT_SECRET = `${process.env.JWT_SECRET}`;
+export const MONGO_URI = process.env.MONGO_URI as string;
+export const JWT_SECRET = process.env.JWT_SECRET as string;
 
-export const TWILIO_ACCT_SID = `${process.env.TWILIO_ACCT_SID}`;
-export const TWILIO_AUTH_TOKEN = `${process.env.TWILIO_AUTH_TOKEN}`;
-export const TWILIO_FROM_NUMBER = `${process.env.TWILIO_FROM_NUMBER}`;
+export const TWILIO_ACCT_SID = process.env.TWILIO_ACCT_SID as string;
+export const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN as string;
+export const TWILIO_FROM_NUMBER = process.env.TWILIO_FROM_NUMBER as string;
